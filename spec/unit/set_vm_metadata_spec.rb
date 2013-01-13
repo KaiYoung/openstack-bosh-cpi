35983a53df2f9ea3aebf387b7e35623d80d3fa10
Ferran Rodenas <frodenas@gmail.com>
2013-01-12 22:05:41 -0800

    Implement set_vm_metadata

---
+++ spec/unit/set_vm_metadata_spec.rb
@@ -0,0 +1,34 @@
+# Copyright (c) 2009-2012 VMware, Inc.
+
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Bosh::OpenStackCloud::Cloud do
+  before :each do
+    @server = double("server", :id => "i-foobar")
+    @metadata = double("metadata")
+
+    @cloud = mock_cloud do |openstack|
+      openstack.servers.should_receive(:get).
+        with("i-foobar").and_return(@server)
+    end
+  end
+
+  it "should set metadata" do
+    metadata = {:job => "job", :index => "index"}
+
+    @server.should_receive(:metadata).and_return(@metadata, @metadata)
+    @metadata.should_receive(:update).with(:job => "job")
+    @metadata.should_receive(:update).with(:index => "index")
+
+    @cloud.set_vm_metadata("i-foobar", metadata)
+  end
+
+  it "should set metadata with a nil value" do
+    metadata = {:deployment => nil}
+
+    @server.should_receive(:metadata).and_return(@metadata)
+    @metadata.should_receive(:update).with(:deployment => "")
+
+    @cloud.set_vm_metadata("i-foobar", metadata)
+  end
+end
